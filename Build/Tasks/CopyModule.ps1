282b309ae71aa36a5b185ec4761ba72cac83d579
Raimund Andée [MSFT] <raandree@live.com>
2019-01-19 20:21:18 +0100

    Lots of update of build scripts

--- Build/Tasks/CopyModule.ps1
+++ Build/Tasks/CopyModule.ps1
@@ -4,8 +4,19 @@ task CopyModule {
     if ($env:BHBuildSystem -eq 'AppVeyor') {
         Update-Metadata -Path $env:BHPSModuleManifest -Verbose -Value $env:APPVEYOR_BUILD_VERSION
     }
+    elseif ($env:BHBuildSystem -eq 'VSTS') {
+        $currentVersion = Get-Metadata -Path $env:BHPSModuleManifest -PropertyName ModuleVersion
+        
+        $newVersion = $currentVersion.Split('.')
+        $newVersion[-1] = $env:BHBuildNumber
+        $newVersion = $newVersion -join '.'
 
-    Write-Build Green "Copy folder '$projectPath\Modules\$($env:BHProjectName)' to '$buildOutput'" Green
-    Copy-Item -Path $projectPath\Modules\$env:BHProjectName -Destination $buildOutput\Modules -Recurse -Force
+        Write-Build Green "Current version is '$currentVersion', new version will be '$newVersion'"
+
+        Update-Metadata -Path $env:BHPSModuleManifest -Verbose -Value $newVersion
+    }
+
+    Write-Build Green "Copy folder '$($projectPath.FullName)\$($env:BHProjectName)' to '$buildOutput'" Green
+    Copy-Item -Path "$($projectPath.FullName)\$($env:BHProjectName)" -Destination $buildOutput\Modules -Recurse -Force
 
 }
